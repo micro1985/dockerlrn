a37ec2477bc21cae7b1603bddb54887a8afcd23d
Sasha Gatsiha <micro1985@mail.ru>
2021-06-17 11:17:00 +0300

    start kuber with Astahov

---
+++ advit/dckrimg/index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from Kuber</h1><p>";
+echo "Sever IP address is: $ip_server", "<br><p>";
+echo "Made by <font color=green>Sasha Gatsiha";
+
+?>
