649d8b8a11074aa2b4a5fd76c82fd070f172cfdc
Sasha Gatsiha <micro1985@mail.ru>
2021-06-17 13:58:13 +0300

    learning in progress

--- advit/dckrimg/index.php
+++ advit/dckrimg/index.php
@@ -1,8 +1,7 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
-
 echo "<h1>Hello from Kuber</h1><p>";
-echo "Sever IP address is: $ip_server", "<br><p>";
-echo "Made by <font color=green>Sasha Gatsiha";
+echo "Server IP address is: $ip_server", "<br><p>";
+echo "Made by <font color=green>Sasha Gatsiha"
 
 ?>
