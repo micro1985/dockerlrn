7ce27559953b74769786fe65714660c6bff03780
Sasha Gatsiha <micro1985@mail.ri>
2021-06-28 16:50:09 +0300

    added kuber manifests

--- advit/dckrimg/index.php
+++ advit/dckrimg/index.php
@@ -2,6 +2,7 @@
 $ip_server = $_SERVER['SERVER_ADDR'];
 echo "<h1>Hello from Kuber</h1><p>";
 echo "Server IP address is: $ip_server", "<br><p>";
-echo "Made by <font color=green>Sasha Gatsiha"
+echo "Made by <font color=green>Sasha Gatsiha", "<br><p>";
+echo "<font color=red>VERSION_1", "<br><p>";
 
 ?>
